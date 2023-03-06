b4212a0ee9a885e85462ae1909141e11eb4f8777
KURAKUSURI <kurakusurendra267@gmail.com>
2023-03-06 22:59:33 +0530

    first commit

---
+++ Suri/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+# Create your views here.
+
+def first(request):
+    return HttpResponse('I want a job is important for life')
+
+def nanda(request):
+    return HttpResponse('I want Job any for that in successfully')
